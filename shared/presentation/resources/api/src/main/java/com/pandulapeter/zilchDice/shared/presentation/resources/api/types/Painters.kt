5f501de6ec7e3b678047a3825dc3eefc8d726ca9
Pandula Péter <pandulapeter@gmail.com>
2024-01-29 19:42:18 +0200

    Add rendered image resources.

--- shared/presentation/resources/api/src/main/java/com/pandulapeter/zilchDice/shared/presentation/resources/api/types/Painters.kt
+++ shared/presentation/resources/api/src/main/java/com/pandulapeter/zilchDice/shared/presentation/resources/api/types/Painters.kt
@@ -6,4 +6,82 @@ import androidx.compose.ui.graphics.painter.Painter
 interface Painters {
 
     @get:Composable val dice: Painter
+
+    @get:Composable val dice_1_0: Painter
+    @get:Composable val dice_1_1: Painter
+    @get:Composable val dice_1_2: Painter
+    @get:Composable val dice_1_3: Painter
+    @get:Composable val dice_1_4: Painter
+    @get:Composable val dice_1_5: Painter
+    @get:Composable val dice_1_6: Painter
+    @get:Composable val dice_1_7: Painter
+    @get:Composable val dice_1_8: Painter
+    @get:Composable val dice_1_9: Painter
+    @get:Composable val dice_1_top: Painter
+    @get:Composable val dice_1_topPerspective: Painter
+
+    @get:Composable val dice_2_0: Painter
+    @get:Composable val dice_2_1: Painter
+    @get:Composable val dice_2_2: Painter
+    @get:Composable val dice_2_3: Painter
+    @get:Composable val dice_2_4: Painter
+    @get:Composable val dice_2_5: Painter
+    @get:Composable val dice_2_6: Painter
+    @get:Composable val dice_2_7: Painter
+    @get:Composable val dice_2_8: Painter
+    @get:Composable val dice_2_9: Painter
+    @get:Composable val dice_2_top: Painter
+    @get:Composable val dice_2_topPerspective: Painter
+
+    @get:Composable val dice_3_0: Painter
+    @get:Composable val dice_3_1: Painter
+    @get:Composable val dice_3_2: Painter
+    @get:Composable val dice_3_3: Painter
+    @get:Composable val dice_3_4: Painter
+    @get:Composable val dice_3_5: Painter
+    @get:Composable val dice_3_6: Painter
+    @get:Composable val dice_3_7: Painter
+    @get:Composable val dice_3_8: Painter
+    @get:Composable val dice_3_9: Painter
+    @get:Composable val dice_3_top: Painter
+    @get:Composable val dice_3_topPerspective: Painter
+
+    @get:Composable val dice_4_0: Painter
+    @get:Composable val dice_4_1: Painter
+    @get:Composable val dice_4_2: Painter
+    @get:Composable val dice_4_3: Painter
+    @get:Composable val dice_4_4: Painter
+    @get:Composable val dice_4_5: Painter
+    @get:Composable val dice_4_6: Painter
+    @get:Composable val dice_4_7: Painter
+    @get:Composable val dice_4_8: Painter
+    @get:Composable val dice_4_9: Painter
+    @get:Composable val dice_4_top: Painter
+    @get:Composable val dice_4_top_perspective: Painter
+
+    @get:Composable val dice_5_0: Painter
+    @get:Composable val dice_5_1: Painter
+    @get:Composable val dice_5_2: Painter
+    @get:Composable val dice_5_3: Painter
+    @get:Composable val dice_5_4: Painter
+    @get:Composable val dice_5_5: Painter
+    @get:Composable val dice_5_6: Painter
+    @get:Composable val dice_5_7: Painter
+    @get:Composable val dice_5_8: Painter
+    @get:Composable val dice_5_9: Painter
+    @get:Composable val dice_5_top: Painter
+    @get:Composable val dice_5_topPerspective: Painter
+
+    @get:Composable val dice_6_0: Painter
+    @get:Composable val dice_6_1: Painter
+    @get:Composable val dice_6_2: Painter
+    @get:Composable val dice_6_3: Painter
+    @get:Composable val dice_6_4: Painter
+    @get:Composable val dice_6_5: Painter
+    @get:Composable val dice_6_6: Painter
+    @get:Composable val dice_6_7: Painter
+    @get:Composable val dice_6_8: Painter
+    @get:Composable val dice_6_9: Painter
+    @get:Composable val dice_6_top: Painter
+    @get:Composable val dice_6_topPerspective: Painter
 }
--- shared/presentation/resources/implementation/src/commonMain/kotlin/com/pandulapeter/zilchDice/shared/presentation/resources/implementation/types/PaintersImpl.kt
+++ shared/presentation/resources/implementation/src/commonMain/kotlin/com/pandulapeter/zilchDice/shared/presentation/resources/implementation/types/PaintersImpl.kt
@@ -1,6 +1,7 @@
 package com.pandulapeter.zilchDice.shared.presentation.resources.implementation.types
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
 import com.pandulapeter.zilchDice.shared.presentation.resources.api.types.Painters
 import com.pandulapeter.zilchDice.shared.presentation.resources.implementation.MR
 import dev.icerock.moko.resources.compose.painterResource
@@ -8,4 +9,82 @@ import dev.icerock.moko.resources.compose.painterResource
 internal class PaintersImpl : Painters {
 
     override val dice @Composable get() = painterResource(MR.images.img_dice)
+
+    override val dice_1_0: Painter @Composable get() = painterResource(MR.images.img_dice_1_0)
+    override val dice_1_1: Painter @Composable get() = painterResource(MR.images.img_dice_1_1)
+    override val dice_1_2: Painter @Composable get() = painterResource(MR.images.img_dice_1_2)
+    override val dice_1_3: Painter @Composable get() = painterResource(MR.images.img_dice_1_3)
+    override val dice_1_4: Painter @Composable get() = painterResource(MR.images.img_dice_1_4)
+    override val dice_1_5: Painter @Composable get() = painterResource(MR.images.img_dice_1_5)
+    override val dice_1_6: Painter @Composable get() = painterResource(MR.images.img_dice_1_6)
+    override val dice_1_7: Painter @Composable get() = painterResource(MR.images.img_dice_1_7)
+    override val dice_1_8: Painter @Composable get() = painterResource(MR.images.img_dice_1_8)
+    override val dice_1_9: Painter @Composable get() = painterResource(MR.images.img_dice_1_9)
+    override val dice_1_top: Painter @Composable get() = painterResource(MR.images.img_dice_1_top)
+    override val dice_1_topPerspective: Painter @Composable get() = painterResource(MR.images.img_dice_1_top_perspective)
+
+    override val dice_2_0: Painter @Composable get() = painterResource(MR.images.img_dice_2_0)
+    override val dice_2_1: Painter @Composable get() = painterResource(MR.images.img_dice_2_1)
+    override val dice_2_2: Painter @Composable get() = painterResource(MR.images.img_dice_2_2)
+    override val dice_2_3: Painter @Composable get() = painterResource(MR.images.img_dice_2_3)
+    override val dice_2_4: Painter @Composable get() = painterResource(MR.images.img_dice_2_4)
+    override val dice_2_5: Painter @Composable get() = painterResource(MR.images.img_dice_2_5)
+    override val dice_2_6: Painter @Composable get() = painterResource(MR.images.img_dice_2_6)
+    override val dice_2_7: Painter @Composable get() = painterResource(MR.images.img_dice_2_7)
+    override val dice_2_8: Painter @Composable get() = painterResource(MR.images.img_dice_2_8)
+    override val dice_2_9: Painter @Composable get() = painterResource(MR.images.img_dice_2_9)
+    override val dice_2_top: Painter @Composable get() = painterResource(MR.images.img_dice_2_top)
+    override val dice_2_topPerspective: Painter @Composable get() = painterResource(MR.images.img_dice_2_top_perspective)
+
+    override val dice_3_0: Painter @Composable get() = painterResource(MR.images.img_dice_3_0)
+    override val dice_3_1: Painter @Composable get() = painterResource(MR.images.img_dice_3_1)
+    override val dice_3_2: Painter @Composable get() = painterResource(MR.images.img_dice_3_2)
+    override val dice_3_3: Painter @Composable get() = painterResource(MR.images.img_dice_3_3)
+    override val dice_3_4: Painter @Composable get() = painterResource(MR.images.img_dice_3_4)
+    override val dice_3_5: Painter @Composable get() = painterResource(MR.images.img_dice_3_5)
+    override val dice_3_6: Painter @Composable get() = painterResource(MR.images.img_dice_3_6)
+    override val dice_3_7: Painter @Composable get() = painterResource(MR.images.img_dice_3_7)
+    override val dice_3_8: Painter @Composable get() = painterResource(MR.images.img_dice_3_8)
+    override val dice_3_9: Painter @Composable get() = painterResource(MR.images.img_dice_3_9)
+    override val dice_3_top: Painter @Composable get() = painterResource(MR.images.img_dice_3_top)
+    override val dice_3_topPerspective: Painter @Composable get() = painterResource(MR.images.img_dice_3_top_perspective)
+
+    override val dice_4_0: Painter @Composable get() = painterResource(MR.images.img_dice_4_0)
+    override val dice_4_1: Painter @Composable get() = painterResource(MR.images.img_dice_4_1)
+    override val dice_4_2: Painter @Composable get() = painterResource(MR.images.img_dice_4_2)
+    override val dice_4_3: Painter @Composable get() = painterResource(MR.images.img_dice_4_3)
+    override val dice_4_4: Painter @Composable get() = painterResource(MR.images.img_dice_4_4)
+    override val dice_4_5: Painter @Composable get() = painterResource(MR.images.img_dice_4_5)
+    override val dice_4_6: Painter @Composable get() = painterResource(MR.images.img_dice_4_6)
+    override val dice_4_7: Painter @Composable get() = painterResource(MR.images.img_dice_4_7)
+    override val dice_4_8: Painter @Composable get() = painterResource(MR.images.img_dice_4_8)
+    override val dice_4_9: Painter @Composable get() = painterResource(MR.images.img_dice_4_9)
+    override val dice_4_top: Painter @Composable get() = painterResource(MR.images.img_dice_4_top)
+    override val dice_4_top_perspective: Painter @Composable get() = painterResource(MR.images.img_dice_4_top_perspective)
+
+    override val dice_5_0: Painter @Composable get() = painterResource(MR.images.img_dice_5_0)
+    override val dice_5_1: Painter @Composable get() = painterResource(MR.images.img_dice_5_1)
+    override val dice_5_2: Painter @Composable get() = painterResource(MR.images.img_dice_5_2)
+    override val dice_5_3: Painter @Composable get() = painterResource(MR.images.img_dice_5_3)
+    override val dice_5_4: Painter @Composable get() = painterResource(MR.images.img_dice_5_4)
+    override val dice_5_5: Painter @Composable get() = painterResource(MR.images.img_dice_5_5)
+    override val dice_5_6: Painter @Composable get() = painterResource(MR.images.img_dice_5_6)
+    override val dice_5_7: Painter @Composable get() = painterResource(MR.images.img_dice_5_7)
+    override val dice_5_8: Painter @Composable get() = painterResource(MR.images.img_dice_5_8)
+    override val dice_5_9: Painter @Composable get() = painterResource(MR.images.img_dice_5_9)
+    override val dice_5_top: Painter @Composable get() = painterResource(MR.images.img_dice_5_top)
+    override val dice_5_topPerspective: Painter @Composable get() = painterResource(MR.images.img_dice_5_top_perspective)
+
+    override val dice_6_0: Painter @Composable get() = painterResource(MR.images.img_dice_6_0)
+    override val dice_6_1: Painter @Composable get() = painterResource(MR.images.img_dice_6_1)
+    override val dice_6_2: Painter @Composable get() = painterResource(MR.images.img_dice_6_2)
+    override val dice_6_3: Painter @Composable get() = painterResource(MR.images.img_dice_6_3)
+    override val dice_6_4: Painter @Composable get() = painterResource(MR.images.img_dice_6_4)
+    override val dice_6_5: Painter @Composable get() = painterResource(MR.images.img_dice_6_5)
+    override val dice_6_6: Painter @Composable get() = painterResource(MR.images.img_dice_6_6)
+    override val dice_6_7: Painter @Composable get() = painterResource(MR.images.img_dice_6_7)
+    override val dice_6_8: Painter @Composable get() = painterResource(MR.images.img_dice_6_8)
+    override val dice_6_9: Painter @Composable get() = painterResource(MR.images.img_dice_6_9)
+    override val dice_6_top: Painter @Composable get() = painterResource(MR.images.img_dice_6_top)
+    override val dice_6_topPerspective: Painter @Composable get() = painterResource(MR.images.img_dice_6_top_perspective)
 }
